abd4b3a74c801fdcd6ecad1657ed1a1c59f820a4
Qylahrzk <nuraqilahnur@gmail.com>
2025-01-26 11:51:50 +0800

    Update main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,149 @@
 import 'package:flutter/material.dart';
+// ignore: unnecessary_import
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:moneyflow/screens/log_expenses_screen.dart';
+import 'package:moneyflow/screens/account_screen.dart';
+import 'package:moneyflow/screens/login_screen.dart';
+import 'package:moneyflow/screens/edit_profile_screen.dart';
+import 'package:moneyflow/screens/settings_screen.dart';
+import 'package:moneyflow/screens/signup_screen.dart';
+import 'package:moneyflow/screens/budget_tracking_screen.dart';
+import 'package:moneyflow/screens/security_screen.dart'; // New route for Security
 
-void main() {
-  runApp(const MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+  // Initialize Hive and open necessary boxes
+  await Hive.initFlutter();
+  await Hive.openBox('expenses');
+  await Hive.openBox('user');
+  await Hive.openBox('settings');
+
+
+  runApp(const MoneyFlowApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+class MoneyFlowApp extends StatelessWidget {
+  const MoneyFlowApp({super.key});
+
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    final settingsBox = Hive.box('settings');
+    bool isDarkMode = settingsBox.get('darkMode', defaultValue: false);
+
+
+    final userBox = Hive.box('user');
+    bool isLoggedIn = userBox.containsKey('email');
+
+
+    return ValueListenableBuilder(
+      valueListenable: settingsBox.listenable(),
+      builder: (context, box, child) {
+        isDarkMode = box.get('darkMode', defaultValue: false);
+
+
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'MoneyFlow',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            brightness: Brightness.light,
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.purpleAccent,
+              foregroundColor: Colors.white,
+            ),
+          ),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            primarySwatch: Colors.purple,
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.black,
+              foregroundColor: Colors.purpleAccent,
+            ),
+          ),
+          themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+          initialRoute: isLoggedIn ? '/home' : '/login',
+          routes: {
+            '/login': (context) => const LoginScreen(),
+            '/signup': (context) => const SignupScreen(),
+            '/home': (context) => const HomeScreen(),
+            '/edit-profile': (context) => const EditProfileScreen(currentName: '',),
+            '/settings': (context) => const SettingsScreen(),
+            '/security': (context) => const SecurityScreen(), // Add Security route
+          },
+        );
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  // ignore: library_private_types_in_public_api
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+
+  final List<Widget> _pages = [
+    const LogExpensesScreen(),
+    const BudgetTrackingScreen(expensesByCategory: {}, budgets: {}, totalBudget: 0.0, totalExpenses: 0.0,),
+    const AccountScreen(),
+  ];
+
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: Text(
+          _selectedIndex == 0
+              ? 'MoneyFlow - Expenses'
+              : _selectedIndex == 1
+                  ? 'Budget Tracking'
+                  : 'Account',
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.yellow,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.attach_money),
+            label: 'Expenses',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.track_changes),
+            label: 'Budget Tracking',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Account',
+          ),
+        ],
+      ),
     );
   }
 }
+
